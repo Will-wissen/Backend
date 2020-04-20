4d25407bfe13596d1f39c576b9c699257dcdd26f
Florian Safer <fsafer@student.tgm.ac.at>
2020-04-20 17:36:42 +0200

    Created entity Stunde

--- database/src/main/java/w3d/database/entitys/Ort.java
+++ database/src/main/java/w3d/database/entitys/Ort.java
@@ -1,4 +1,49 @@
 package w3d.database.entitys;
 
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Ort")
 public class Ort {
+
+    @Id
+    private String name;
+
+    private String beschreibung;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "Stunde",cascade = CascadeType.ALL)
+    private Stunde stunde;
+
+    public Ort(){}
+
+    public Ort(String name, String beschreibung){
+        this.name = name;
+        this.beschreibung = beschreibung;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Stunde getStunde() {
+        return stunde;
+    }
+
+    public void setStunde(Stunde stunde) {
+        this.stunde = stunde;
+    }
 }
--- database/src/main/java/w3d/database/entitys/Stunde.java
+++ database/src/main/java/w3d/database/entitys/Stunde.java
@@ -1,4 +1,63 @@
 package w3d.database.entitys;
 
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Entity
+@Table(name = "Stunde")
 public class Stunde {
+
+    @Id
+    private Time azeit;
+
+    @Id
+    private Time ezeit;
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Ort ort;
+
+    private String bezeichnung;
+
+    public Stunde(){}
+
+    public Stunde(Time azeit, Time ezeit, Ort ort, String bezeichnung){
+        this.azeit = azeit;
+        this.ezeit = ezeit;
+        this.ort = ort;
+        this.bezeichnung = bezeichnung;
+    }
+
+    public Time getAzeit() {
+        return azeit;
+    }
+
+    public void setAzeit(Time azeit) {
+        this.azeit = azeit;
+    }
+
+    public Time getEzeit() {
+        return ezeit;
+    }
+
+    public void setEzeit(Time ezeit) {
+        this.ezeit = ezeit;
+    }
+
+    public Ort getOrt() {
+        return ort;
+    }
+
+    public void setOrt(Ort ort) {
+        this.ort = ort;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
 }
