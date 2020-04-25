f5111086fdbcf3fd6a9499bac240a5bb417f4cc1
Florian Safer <fsafer@student.tgm.ac.at>
2020-04-25 12:14:27 +0200

    Started Anwesenheit, not finished

--- database/src/main/java/w3d/database/entitys/Anwesenheit.java
+++ database/src/main/java/w3d/database/entitys/Anwesenheit.java
@@ -1,4 +1,15 @@
 package w3d.database.entitys;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Anwesenheit")
 public class Anwesenheit {
+
+    
+
 }
